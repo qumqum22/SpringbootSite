99e34d31af4aba9eac7ac8aef431c3b300d9b99f
Piotr Gucwa <piotr.gucwa@student.pk.edu.pl>
2021-06-10 12:46:31 +0200

    Navigation repaired

--- Frontend/src/app/home/profile/profile.component.ts
+++ Frontend/src/app/home/profile/profile.component.ts
@@ -17,28 +17,54 @@ export class ProfileComponent implements OnInit {
   user: User;
   editUser: User;
   phones: Phone[];
-  signature: string = "";
+  titleField: string ="";
+  nameField:string = "";
+  surnameField: string = "";
 
 
-  constructor(private userService: UserService, private phoneService: PhoneService) { }
 
-  public getUser():void {
-    this.userService.getUser(this.currentUser).subscribe(
-      (response: User) => {
-        this.user = response;
-        this.signature = this.getSignature();
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    )
+    constructor(private userService: UserService, private phoneService: PhoneService) { }
+
+    ngOnInit(): void {
+      this.getUser();
+      this.getPhones();
+    }
+
+    getUser():void {
+      this.userService.getUser(this.currentUser).subscribe(
+        (response: User) => {
+          this.user = response;
+          this.getSignature();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      )
+    }
+
+    getSignature(){
+    this.titleField = this.user.title;
+    this.nameField = this.user.name;
+    this.surnameField = this.user.surname;
   }
+    updateUser():void{
+      this.user.title = this.titleField;
+      this.user.name = this.nameField;
+      this.user.surname = this.surnameField;
+      console.log(this.user);
+      this.userService.updateUser(this.user).subscribe(
+        (response: User) => {
+          console.log(response);
+          this.getPhones();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+    }
 
-  public getSignature(){
-    return `${this.user.title} ${this.user.name} ${this.user.surname}`;
-  }
 
-    public deletePhone(phoneId: number):void {
+    deletePhone(phoneId: number):void {
       this.phoneService.deletePhone(phoneId).subscribe(
         (response: void) => {
           console.log(response);
@@ -51,7 +77,7 @@ export class ProfileComponent implements OnInit {
     }
 
       // get user's phones, powinno byc zalezne od id uzytkownika
-  public getPhones():void {
+  getPhones():void {
     this.phoneService.getPhones().subscribe(
       (response: Phone[]) => {
         this.phones = response;
@@ -63,7 +89,7 @@ export class ProfileComponent implements OnInit {
     )
   }
 
-    public addPhone(addPhoneForm: NgForm):void {
+    addPhone(addPhoneForm: NgForm):void {
       console.log(addPhoneForm.value);
       this.phoneService.addPhone(addPhoneForm.value, this.user.id).subscribe(
         (response: Phone) => {
@@ -75,30 +101,6 @@ export class ProfileComponent implements OnInit {
         }
       )
     }
-    updateUser(){
-      const parts = this.signature.split(" ");
-      this.user.title = parts[0];
-      this.user.name = parts[1];
-      this.user.surname = parts[2];
-      console.log(this.user);
-      this.userService.updateUser(this.user);
-    }
 
-    // public editDescription(editDescriptionForm: NgForm):void {
-    //   console.log(editDescriptionForm.value);
-    //   this.userService.editDescription(editDescriptionForm.value).subscribe(
-    //     (response: User) => {
-    //       console.log(response);
-    //       this.getUser();
-    //     },
-    //     (error: HttpErrorResponse) => {
-    //       alert(error.message);
-    //     }
-    //   )
-    // }
 
-  ngOnInit(): void {
-    this.getUser();
-    this.getPhones();
-  }
 }
